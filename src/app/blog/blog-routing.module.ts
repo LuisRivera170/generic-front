f11ebb419679a3d3f2a9cceecaf4aee3372e7c80
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-04-05 21:43:31 -0500

    Adding router-outlet to layout component

--- src/app/blog/blog-routing.module.ts
+++ src/app/blog/blog-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { LayoutComponent } from './components/layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
   }
 ];
 
--- src/app/blog/components/layout/layout.component.ts
+++ src/app/blog/components/layout/layout.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   template: `<app-base-layout>
+            <router-outlet></router-outlet>
             </app-base-layout>`,
 })
 export class LayoutComponent implements OnInit {

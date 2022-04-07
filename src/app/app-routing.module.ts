89fb38fa5e9cb125646ee2865344caa7d0097af9
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-04-06 22:56:10 -0500

    Changing blog module for web module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./blog/blog.module').then(module => module.BlogModule)
+    loadChildren: () => import('./web/web.module').then(module => module.WebModule)
   }
 ];
 
--- src/app/web/web.module.ts
+++ src/app/web/web.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { BlogRoutingModule } from './blog-routing.module';
+import { WebRoutingModule } from './web-routing.module';
 import { ThemeModule } from '../theme/theme.module';
 import { HomeComponent } from './components/home/home.component';
 import { LayoutComponent } from './components/layout/layout.component';
@@ -14,8 +14,8 @@ import { LayoutComponent } from './components/layout/layout.component';
   ],
   imports: [
     CommonModule,
-    BlogRoutingModule,
+    WebRoutingModule,
     ThemeModule
   ]
 })
-export class BlogModule { }
+export class WebModule { }

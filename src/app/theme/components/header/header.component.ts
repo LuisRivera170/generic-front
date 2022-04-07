dd245bb70e34de3744fb6d46913af0d062773c59
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2022-04-06 22:48:27 -0500

    Changing theme

--- src/app/theme/components/header/header.component.ts
+++ src/app/theme/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { NbMenuService } from '@nebular/theme';
+import { NbMenuService, NbThemeService } from '@nebular/theme';
 
 @Component({
   selector: 'app-header',
@@ -10,15 +10,30 @@ import { NbMenuService } from '@nebular/theme';
 export class HeaderComponent implements OnInit {
 
   public titleHeader = environment.appName;
+  public themeIcon = 'sun-outline';
 
-  constructor(private menuService: NbMenuService) { }
+  constructor(private menuService: NbMenuService,
+              private themeService: NbThemeService) { }
 
   ngOnInit(): void {
+    this.themeService
+      .onThemeChange()
+      .subscribe((theme: any) => {
+        this.themeIcon = theme.name === 'default' ? 
+          'sun-outline' : 
+          'moon-outline';
+      });
   }
 
   navigateHome() {
     this.menuService.navigateHome();
     return false;
+  }
+
+  toggleTheme() {
+    this.themeService.currentTheme === 'default' ?
+      this.themeService.changeTheme('dark') :
+      this.themeService.changeTheme('default');
   }
 
 }
--- src/app/theme/theme.module.ts
+++ src/app/theme/theme.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NbLayoutModule } from '@nebular/theme';
+import { NbButtonModule, NbIconModule, NbLayoutModule } from '@nebular/theme';
 import { HeaderComponent } from './components/header/header.component';
 import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 
 const NB_MODULES = [
-  NbLayoutModule,
-  NbEvaIconsModule
+  NbButtonModule,
+  NbIconModule,
+  NbEvaIconsModule,
+  NbLayoutModule
 ];
 
 const COMPONENTS = [
